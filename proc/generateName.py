80746d604744b6832f49338dc0ea35fa2410c0bc
Meihao Chen <mc5283@nyu.edu>
2015-10-06 00:55:46 -0400

    modify zipimport

--- proc/generateName.py
+++ proc/generateName.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python
 import zipimport
 importer = zipimport.zipimporter('nltkandnameparser.mod')
-yaml = importer.load_module('nameparser')
+nameparser = importer.load_module('nameparser')
 nltk = importer.load_module('nltk') 
 
-from nameRecognition import *
 import os
 import sys
+#from testnltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk
+#import testnltk
+#from testnameparser.parser import HumanName
 
-for line in sys.stdin:
-    try:
-        _, text = eval(line)
-        text = text.decode("utf-8").encode("ascii","ignore")
-        names = get_human_names(text)
-        if len(names)>0:
-            for i in names:
-                print "%s\t%d"%(i ,1)
-        else:
+def get_human_names(text):
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentt = nltk.ne_chunk(pos, binary = False)
+    person_list = []
+    person = []
+    name = ""
+    
+    for subtree in sentt.subtrees(filter=lambda t: t.label() == 'PERSON'):
+        for leaf in subtree.leaves():
+            person.append(leaf[0])
+        if len(person) > 1: #avoid grabbing lone surnames
+            for part in person:
+                name += part + ' '
+            if name[:-1] not in person_list:
+                person_list.append(name[:-1])
+            name = ''
+        person = []
+    return (person_list)
+def mapper():
+    for line in sys.stdin:
+        try:
+            _, text = eval(line)
+            text = text.decode("utf-8").encode("ascii","ignore")
+            names = get_human_names(text)
+            if len(names)>0:
+                for i in names:
+                    print "%s\t%d"%(i ,1)
+            else:
+                pass
+        except:
             pass
-    except:
-        pass
 
+if __name__=='__main__':
+    mapper()
 
