e46f0f3cb5f9ef2d6a153f81187d6a4900b328fe
yw652 <yw652@nyu.edu>
2015-10-07 16:25:18 -0400

    modify lmr.sh

--- countMapReduce/lmr.sh
+++ countMapReduce/lmr.sh
@@ -3,8 +3,8 @@
 # confused with the lmr software
 
 cat ./*.json | ./countMapReduce/countUser_map.py | sort | ./countMapReduce/countUser_reduce.py > result_user
-cat ./*.json | ./countMapReduce/countHash_map.py | sort | ./countMapreduce/countHash_reduce.py
-cat ./*.json | ./countMapReduce/countLoca_map.py | sort | ./countMapReduce/countLoca_reduce.py
-cat ./*.json | ./countMapReduce/countMention_map.py | sort | ./countMapReduce/countMention_reduce.py
-cat ./*.json | ./countMapReduce/countTweet_map.py | sort | ./countMapReduce/countTweet_reduce.py
+cat ./*.json | ./countMapReduce/countHash_map.py | sort | ./countMapreduce/countHash_reduce.py > result_hash
+cat ./*.json | ./countMapReduce/countLoca_map.py | sort | ./countMapReduce/countLoca_reduce.py > result_loca
+cat ./*.json | ./countMapReduce/countMention_map.py | sort | ./countMapReduce/countMention_reduce.py > result_mention
+cat ./*.json | ./countMapReduce/countTweet_map.py | sort | ./countMapReduce/countTweet_reduce.py > result_tweet
  
